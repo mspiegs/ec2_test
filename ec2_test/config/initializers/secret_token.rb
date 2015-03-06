fc2384151cc9e23824e22d71c5a9922678c8f6c6
mspiegs <matt@mycaseinc.com>
2015-03-06 13:56:53 -0800

    initial commit with posts

---
+++ ec2_test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ec2Test::Application.config.secret_key_base = 'a008e8f5ae408d03836bc2ed52594bf8d8b16cd95958a699722c65c88a3dad5eba7f403e4d74819e12064e22df0976b04f3611023232f19ba6dc27eb3e4122c9'
---
+++ ec2_test/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ec2Test::Application.config.session_store :cookie_store, key: '_ec2_test_session'
